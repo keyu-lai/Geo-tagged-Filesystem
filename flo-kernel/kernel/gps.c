0840afd7e18966a0e86a796225b5f144e982a018
W4118 Student <w4118@w4118>
2015-12-12 16:54:13 -0500

    updated userspace

--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -6,6 +6,8 @@
 #include <linux/kernel.h>
 #include <linux/uaccess.h>
 #include <linux/syscalls.h>
+#include <linux/errno.h>
+#include <linux/dcache.h>
 
 static DEFINE_RWLOCK(cur_gloc_lock);
 
@@ -43,7 +45,6 @@ SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc) {
 	upd_tim_stm = CURRENT_TIME_SEC.tv_sec;
 	write_unlock(&cur_gloc_lock);
 
-	printk(KERN_DEBUG "long:%d unsigned long:%d int:%d unsigned int:%d\n",sizeof(long), sizeof(unsigned long),sizeof(int), sizeof(unsigned int));
 	return 0;
 }
 
@@ -51,26 +52,52 @@ SYSCALL_DEFINE2(get_gps_location, const char __user *, path_name, struct gps_loc
 	struct gps_location tmp_loc;
 	struct path fp;
 	struct inode *inode;
-	int cage;
+	//int luf;
 	int status;
+	int cage = 0;
+	long accuracy;
+	char *from;
 
-	if (loc == NULL)
+	printk(KERN_DEBUG "Enter get_gps_location syscall\n");
+	memcpy(&accuracy, &cur_gloc.accuracy,sizeof(accuracy));
+	printk(KERN_DEBUG "Accuracy:%ld\n",accuracy);
+
+	if (loc == NULL) {
+		printk(KERN_DEBUG "NULL loc\n");
 		return -EINVAL;
+	}
 
-	if (accuracy == -1)
+	if (accuracy == -1) {
+		printk(KERN_DEBUG "wrong accuracy\n");
 		return -ENODEV;
+	}
+	
+	from = getname(path_name);
+	if (IS_ERR(from)) {
+		printk(KERN_DEBUG "Wrong name!");
+	}
 
-	if (user_path_at(AT_FDCWD, path_name, AT_SYMLINK_FOLLOW | !LOOKUP_FOLLOW, &fp))
+	printk("%s::%d\n",path_name,strlen(path_name));
+	//luf = 0;
+	//luf |= (LOOKUP_FOLLOW);
+	//if (user_path_at(AT_FDCWD, path_name, luf, &fp) < 0) {
+	if (kern_path(path_name, LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT, &fp) != 0) {
+		printk(KERN_DEBUG "wrong path\n");
 		return -EFAULT;
+	}
 
 	inode = fp.dentry->d_inode;
-	if(inode_permission(inode, MAY_READ) < 0)
+	if(inode_permission(inode, MAY_READ) < 0) {
+		printk(KERN_DEBUG "No Permission\n");
 		return -EACCES;
-	
+	}
+
 	cage = vfs_get_gps_location(inode, &tmp_loc);
 	status = copy_to_user(loc, &tmp_loc, sizeof(tmp_loc));
-	if (status < 0)
+	if (status < 0) {
+		printk(KERN_DEBUG "Negative Status\n");
 		return -EFAULT;
+	}
 
 	return cage;
 }
--- userspace/file_loc/file_loc.c
+++ userspace/file_loc/file_loc.c
@@ -3,36 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define GPS_LOCATION_FILE "/data/media/0/gps_location.txt"
 #define STR_LEN 200
 
 int main(int argc, char **argv)
 {
-	char *opt;
+	char *opt, *path;
 	char copt[STR_LEN];
-	int dage, i;
-	size_t nbytes = STR_LEN;
+	int dage, i, slen;
+	size_t br = STR_LEN;
 	struct gps_location rloc;
 
 	opt = (char *)malloc(STR_LEN * sizeof(char));
 
-	while (strcmp(copt, "exit") != 0) {
+	while (strncmp(copt, "exit", 4) != 0) {
 		printf("Enter path name: ");
-		getline(&opt, &nbytes, stdin);
+		slen = getline(&opt, &br, stdin);
 		
-		dage = get_gps_location(opt,&rloc);
-		
-		printf("File name: %s\n",opt);
-		printf("Created at lat : %f\n",rloc.latitude);
-		printf("Created at lod : %f\n",rloc.longitude);
-		printf("Data Accuracy: %f\n",rloc.accuracy);
-		printf("Data age: %d\n",dage);
+		if (slen > 0) {
+			path = (char *)malloc(slen * sizeof(char));
+			strncpy(path, opt, slen - 1);
+			path[slen - 1] = '\0';
+			printf("Path: %s\n",path);
+
+			dage = get_gps_location(path,&rloc);
+			free(path);
+		}
 
 		i = 0;
 		while (i < STR_LEN) {
 			copt[i] = tolower(opt[i]);
 			i++;
+		}
+
+		if (dage < 0 && strncmp(copt, "exit", 4) != 0) {
+			printf("Error! Error code is %d.\n",dage);
+			continue;
+		}
+
+		if (strncmp(copt, "exit", 4) != 0) {
+			printf("File name: %s\n",opt);
+			printf("Created at lat : %f\n",rloc.latitude);
+			printf("Created at lod : %f\n",rloc.longitude);
+			printf("Data Accuracy: %f\n",rloc.accuracy);
+			printf("Data age: %d\n",dage);
 		}
 	}
 	free(opt);
