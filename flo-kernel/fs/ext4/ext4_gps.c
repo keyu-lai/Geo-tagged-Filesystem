eb02a2ecf23b33109ef6418682c1a99119d138db
Joel Cretan <jcc2273@columbia.edu>
2015-12-14 17:22:52 -0500

    partial fix for the case of a file on a GPS-aware FS that does not have any GPS data

--- flo-kernel/fs/ext4/ext4_gps.c
+++ flo-kernel/fs/ext4/ext4_gps.c
@@ -29,9 +29,12 @@ long ext4_get_gps_location(struct inode *inode, struct gps_location *location)
 	struct gps_location tmp_loc;
 	struct ext4_inode_info *iinfo = EXT4_I(inode);
 	long coord_age;
+	__u64 longitude, latitude;
+	__u32 accuracy;
 
 	if (!test_opt(inode->i_sb, GPS_AWARE_INODE))
 		return -ENODEV;
+
 
 	read_lock(&iinfo->i_gps_lock);
 	memcpy(&tmp_loc.latitude, &iinfo->i_latitude, sizeof(long long));
@@ -40,6 +43,16 @@ long ext4_get_gps_location(struct inode *inode, struct gps_location *location)
 	memcpy(location, &tmp_loc, sizeof(tmp_loc));
 	coord_age = *(long *) &iinfo->i_coord_age;
 	read_unlock(&iinfo->i_gps_lock);
+
+	/* this is a hack to handle the case of a file on a GPS FS with no data. 
+	if someone really were visit the exact spot (0.0, 0.0) 
+	and their device were accurate down to 0.0, this would
+ 	be wrong. but that seems basically impossible */
+	memcpy(&longitude, &tmp_loc.longitude, sizeof(__u64));
+	memcpy(&latitude, &tmp_loc.latitude, sizeof(__u64));
+	memcpy(&accuracy, &tmp_loc.accuracy, sizeof(__u32));
+	if (latitude == 0 && longitude == 0 && accuracy == 0)
+		return -ENODEV;
 
 	return coord_age;
 }
